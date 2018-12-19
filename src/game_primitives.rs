650e08b0b32a314257570b787922dda45dfd38e2
andy <andysalerno@gmail.com>
2018-12-19 00:43:40 -0800

    begin defining generic game primitives and player logic

--- src/game_primitives.rs
+++ src/game_primitives.rs
@@ -2,34 +2,70 @@ pub enum PlayerColor {
     Black,
     White,
 }
-
 pub enum GameResult {
-    NotOver,
     Tie,
     WhiteWins,
     BlackWins,
 }
 
-pub trait PrintableGameState {}
+/// Describes a move a player can make in a game.
+/// I.e., in Reversi, a move could be at position (3,7).
+pub trait GameMove {}
 
-pub trait PlayableGame {
+/// Describes a complete state of some Game,
+/// such as the board position, the current player's turn,
+/// or any other relevant info.
+pub trait GameState {
+    type Move: GameMove;
+
+    /// Returns a human-friendly string for representing the state.
+    fn human_friendly(&self) -> String;
+
+    /// Returns the possible moves the given player can make for the current state.
+    fn legal_moves(&self, player: PlayerColor) -> Vec<Self::Move>;
+}
+
+pub trait Game {
+    type State: GameState;
+    type WhiteAgent: GameAgent<Self::State>;
+    type BlackAgent: GameAgent<Self::State>;
+
     /// Returns the player whose turn it is.
     fn whose_turn(&self) -> PlayerColor;
 
+    fn white_agent(&self) -> &Self::WhiteAgent;
+    fn black_agent(&self) -> &Self::BlackAgent;
+
+    /// The game's current state.
+    fn game_state(&self) -> &Self::State;
+
+    /// True if the the game has ended, either due to a forced win,
+    /// draw, or forfeit.
     fn is_game_over(&self) -> bool;
 
-    fn game_result(&self) -> GameResult;
+    /// The GameResult, or None if the game is not yet over.
+    fn game_result(&self) -> Option<GameResult>;
 
+    /// Invokes the current player agent to pick a move,
+    /// then updates the game state and gives control to the next player.
     fn player_take_turn(&mut self);
 
-    /// Returns a human-friendly string representation of the current game state.
-    fn printable_state(&self) -> std::convert::Into<&str>;
+    fn play_to_end(&mut self) -> GameResult {
+        while !self.is_game_over() {
+            match self.whose_turn() {
+                PlayerColor::Black => self.player_take_turn(),
+                PlayerColor::White => self.player_take_turn(),
+            }
+        }
+
+        GameResult::BlackWins
+    }
 }
 
-struct Test;
+pub trait GameAgent<TState: GameState> {
+    fn pick_move(state: &TState);
+}
 
-impl PlayableGame for Test {
-    fn whose_turn(&self) -> PlayerColor {
-        PlayerColor::White
-    }
+mod tests {
+    use super::*;
 }
