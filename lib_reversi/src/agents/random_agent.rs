f42968a6ee4345b6593c7ebbc8d78dc78958426a
andy <andysalerno@gmail.com>
2019-01-19 02:48:37 -0800

    make random agent more random

--- lib_reversi/src/agents/random_agent.rs
+++ lib_reversi/src/agents/random_agent.rs
@@ -1,4 +1,5 @@
 use lib_boardgame::game_primitives::{GameAgent, GameState, PlayerColor};
+use rand::seq::SliceRandom;
 
 pub struct RandomAgent {
     color: PlayerColor,
@@ -7,15 +8,23 @@ pub struct RandomAgent {
 impl RandomAgent {
     /// Creates a new RandomAgent, playing for the given color.
     pub fn new(color: PlayerColor) -> Self {
-        RandomAgent { color }
+        RandomAgent {
+            color,
+        }
+    }
+
+    fn random_choice<T>(&self, choices: &[T]) -> T
+    where
+        T: Copy,
+    {
+        *choices.choose(&mut rand::thread_rng()).unwrap()
     }
 }
 
 impl<TState: GameState> GameAgent<TState> for RandomAgent {
     fn pick_move(&self, state: &TState) -> TState::Move {
-        // not very random yet :)
         let legal_moves = dbg!(state.legal_moves(self.color));
 
-        legal_moves[0]
+        self.random_choice(&legal_moves)
     }
 }
