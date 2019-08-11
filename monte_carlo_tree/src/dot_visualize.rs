8d9c24e56fa44e6a2f0bcffc5bbef099016be072
andy <andysalerno@gmail.com>
2019-08-11 14:46:54 -0700

    broken commit: visualizer is busted

--- monte_carlo_tree/src/dot_visualize.rs
+++ monte_carlo_tree/src/dot_visualize.rs
@@ -16,7 +16,7 @@ where
 {
     fn to_dot_file_str(&self, depth_limit: Option<usize>) -> String {
         const HEADER: &str =
-            "digraph prof {\nratio = fill;\nnode [style=filled, shape=box, fontname=consolas];\n";
+            "digraph prof {\nratio = fill;\nnode [style=filled, shape=box, fontname=courier];\n";
         const FOOTER: &str = "}";
 
         let mut node_id_map_buf = String::new();
@@ -54,24 +54,35 @@ fn depth_first_tree_walk<T, TState>(
     let id = hash_str(&label).wrapping_add(path_hash);
 
     let label_str = format!("{} [label = \"{}\"]\n", id, label);
+
+    // Add the label for this node
     node_labels_buf.push_str(&label_str);
 
-    for child in node.children() {
-        let child_label = node_label(child.borrow());
-        let child_id = hash_str(child_label).wrapping_add(id);
+    if depth_remaining.is_none() || depth_remaining.unwrap() > 1 {
+        for child in node.children() {
+            let child_label = node_label(child.borrow());
+            let child_id = hash_str(&child_label).wrapping_add(id);
 
-        let id_mapping_str = format!("{} -> {};\n", id, child_id);
+            if depth_remaining.is_none() {
+                // We're not recursing anymore, so we need to define child labels here
+                let label_str = format!("{} [label = \"{}\"]\n", child_id, child_label);
+                node_labels_buf.push_str(&label_str);
+            }
 
-        node_id_map_buf.push_str(&id_mapping_str);
+            let id_mapping_str = format!("{} -> {};\n", id, child_id);
 
-        if depth_remaining.is_some() {
-            depth_first_tree_walk(
-                child.borrow(),
-                id,
-                node_labels_buf,
-                node_id_map_buf,
-                depth_remaining.and_then(|v| Some(v - 1)),
-            );
+            // Add the mapping from this node to the child node
+            node_id_map_buf.push_str(&id_mapping_str);
+
+            if depth_remaining.is_some() {
+                depth_first_tree_walk(
+                    child.borrow(),
+                    id,
+                    node_labels_buf,
+                    node_id_map_buf,
+                    depth_remaining.and_then(|v| Some(v - 1)),
+                );
+            }
         }
     }
 
