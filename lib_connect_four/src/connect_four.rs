9b03e6945e7e572fecd6a2e6eca6dc7b2ead7578
unknown <andysalerno@gmail.com>
2019-10-20 23:46:33 -0700

    implement connect four, but it has no tests at all yet

--- lib_connect_four/src/connect_four.rs
+++ lib_connect_four/src/connect_four.rs
@@ -4,6 +4,10 @@ use std::fmt::Display;
 const GAME_WIDTH: usize = 7;
 const GAME_HEIGHT: usize = GAME_WIDTH - 1;
 
+fn is_in_range(col: usize, height: usize) -> bool {
+    col < GAME_WIDTH && height < GAME_HEIGHT
+}
+
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum ConnectFourPiece {
     Black,
@@ -25,14 +29,14 @@ impl Display for ConnectFourPiece {
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct ConnectFourAction {
-    /// The row index where the piece will be dropped,
-    /// where 0 is the leftmost row and GAME_SIZE-1 is the rightmost.
-    row: usize,
+    /// The col index where the piece will be dropped,
+    /// where 0 is the leftmost col and GAME_SIZE-1 is the rightmost.
+    col: usize,
 }
 
 impl ConnectFourAction {
-    fn new(row: usize) -> Self {
-        Self { row }
+    fn new(col: usize) -> Self {
+        Self { col }
     }
 }
 
@@ -45,7 +49,7 @@ impl lib_boardgame::GameMove for ConnectFourAction {
 
 impl Display for ConnectFourAction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "row: {}", self.row)
+        write!(f, "col: {}", self.col)
     }
 }
 
@@ -53,60 +57,131 @@ impl Display for ConnectFourAction {
 pub struct ConnectFourState {
     player_turn: PlayerColor,
     board: [[ConnectFourPiece; GAME_WIDTH]; GAME_HEIGHT],
-    row_cur_height: [usize; GAME_WIDTH],
+    col_cur_height: [usize; GAME_WIDTH],
 
     legal_moves: Vec<ConnectFourAction>,
+    game_result: Option<GameResult>,
 }
 
 impl ConnectFourState {
     pub fn new() -> Self {
         Self {
             player_turn: PlayerColor::Black,
-            row_cur_height: [0; GAME_WIDTH],
+            col_cur_height: [0; GAME_WIDTH],
             board: [[ConnectFourPiece::Empty; GAME_WIDTH]; GAME_HEIGHT],
 
             legal_moves: Default::default(),
+            game_result: None,
         }
     }
 
     fn update_legal_moves(&mut self) {
         let legal = (0..GAME_WIDTH)
-            .filter(|&i| !self.is_row_full(i))
+            .filter(|&i| !self.is_col_full(i))
             .map(|i| ConnectFourAction::new(i))
             .collect::<Vec<_>>();
 
         std::mem::replace(&mut self.legal_moves, legal);
     }
 
-    fn piece_at(&self, row: usize, height: usize) -> ConnectFourPiece {
-        self.board[height][row]
+    fn piece_at(&self, col: usize, height: usize) -> ConnectFourPiece {
+        self.board[height][col]
     }
 
-    fn set_piece(&mut self, row: usize, height: usize, piece: ConnectFourPiece) {
-        self.board[height][row] = piece;
+    fn set_piece(&mut self, col: usize, height: usize, piece: ConnectFourPiece) {
+        self.board[height][col] = piece;
     }
 
-    fn row_height(&self, row: usize) -> usize {
-        self.row_cur_height[row]
+    fn col_height(&self, col: usize) -> usize {
+        self.col_cur_height[col]
     }
 
-    fn is_row_full(&self, row: usize) -> bool {
-        self.row_height(row) >= GAME_HEIGHT
+    fn is_col_full(&self, col: usize) -> bool {
+        self.col_height(col) >= GAME_HEIGHT
     }
 
-    pub fn drop_piece(&mut self, row: usize, piece: ConnectFourPiece) {
-        let piece_height = self.row_height(row);
+    fn increment_col(&mut self, col: usize) {
+        self.col_cur_height[col] += 1;
+    }
+
+    pub fn drop_piece(&mut self, col: usize, piece: ConnectFourPiece) {
+        let piece_height = self.col_height(col);
 
         if piece_height >= GAME_HEIGHT {
             panic!(
-                "can't legally drop a piece in row {}, \
+                "can't legally drop a piece in col {}, \
                  which already has height {} and has no more room.",
-                row, piece_height
+                col, piece_height
             );
         }
 
-        self.set_piece(row, piece_height, piece);
-        self.row_cur_height[row] += 1;
+        self.set_piece(col, piece_height, piece);
+        self.increment_col(col);
+
+        let pos = Position {
+            x: col,
+            y: piece_height,
+        };
+
+        if self.is_pos_four_in_a_row(pos) {
+            self.game_result = match piece {
+                ConnectFourPiece::Black => Some(GameResult::BlackWins),
+                ConnectFourPiece::Red => Some(GameResult::WhiteWins),
+                _ => {
+                    panic!("Can only consider a game won if a black or red piece was lost dropped")
+                }
+            };
+        }
+    }
+
+    fn is_pos_four_in_a_row(&self, pos: Position) -> bool {
+        if self.piece_at(pos.x, pos.y) == ConnectFourPiece::Empty {
+            return false;
+        }
+
+        // Left, right
+        let col_dirs = &[-1, 0, 1];
+
+        // Down, up
+        let height_dirs = &[-1, 0, 1];
+
+        for &col_dir in col_dirs {
+            for &height_dir in height_dirs {
+                if col_dir == 0 && height_dir == 0 {
+                    // No net movement, so skip this one
+                    continue;
+                }
+
+                let origin_color = self.piece_at(pos.x, pos.y);
+
+                let mut dir_run_len = 1;
+
+                // Traverse the direction forwards and backwards
+                for s in &[-1, 1] {
+                    for i in 1.. {
+                        let col: i32 = pos.x as i32 + (s * i * col_dir);
+                        let height: i32 = pos.y as i32 + (s * i * height_dir);
+
+                        if col < 0 || height < 0 || !is_in_range(col as usize, height as usize) {
+                            break;
+                        }
+
+                        if self.piece_at(col as usize, height as usize) == origin_color {
+                            dir_run_len += 1;
+                        } else {
+                            // The single-color run has ended, so stop traversing
+                            break;
+                        }
+                    }
+                }
+
+                if dir_run_len >= 4 {
+                    return true;
+                }
+            }
+        }
+
+        false
     }
 }
 
@@ -114,11 +189,11 @@ impl Display for ConnectFourState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut result = String::new();
 
-        for row in (0..GAME_HEIGHT).rev() {
+        for height in (0..GAME_HEIGHT).rev() {
             result.push('|');
 
-            for row_idx in 0..GAME_WIDTH {
-                let piece = self.piece_at(row_idx, row);
+            for col in 0..GAME_WIDTH {
+                let piece = self.piece_at(col, height);
                 result.push_str(&format!("{}", piece));
                 result.push('|');
             }
@@ -127,8 +202,8 @@ impl Display for ConnectFourState {
         }
 
         result.push(' ');
-        for row in 0..GAME_WIDTH {
-            result.push_str(&format!("{} ", row.to_string()));
+        for col in 0..GAME_WIDTH {
+            result.push_str(&format!("{} ", col.to_string()));
         }
 
         write!(f, "{}", result)
@@ -143,9 +218,9 @@ impl GameState for ConnectFourState {
     }
 
     fn initialize_board(&mut self) {
-        for row in &mut self.board {
-            for row_loc in row {
-                *row_loc = ConnectFourPiece::Empty;
+        for height_slice in &mut self.board {
+            for pos in height_slice {
+                *pos = ConnectFourPiece::Empty;
             }
         }
     }
@@ -162,14 +237,14 @@ impl GameState for ConnectFourState {
     }
 
     fn apply_move(&mut self, action: Self::Move) {
-        let row = action.row;
+        let col = action.col;
 
         let piece = match self.current_player_turn() {
             PlayerColor::Black => ConnectFourPiece::Black,
             PlayerColor::White => ConnectFourPiece::Red,
         };
 
-        self.drop_piece(row, piece);
+        self.drop_piece(col, piece);
 
         self.player_turn = self.player_turn.opponent();
 
@@ -189,7 +264,7 @@ impl GameState for ConnectFourState {
     }
 
     fn is_game_over(&self) -> bool {
-        (0..GAME_WIDTH).all(|i| self.is_row_full(i))
+        self.game_result.is_some()
     }
 }
 
@@ -241,6 +316,11 @@ impl Game for ConnectFour {
 
     /// The GameResult, or None if the game is not yet over.
     fn game_result(&self) -> Option<GameResult> {
-        Some(GameResult::BlackWins)
+        self.game_state.game_result
     }
+}
+
+struct Position {
+    x: usize,
+    y: usize,
 }
